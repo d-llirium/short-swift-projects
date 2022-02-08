7b1781b34694e8384aae8d7618b461ff38f93fba
d-llirium <plimacarlos@gmail.com>
2022-02-08 17:48:29 -0300

    add iamrich

---
+++ i-am-rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Patricia de Lima Carlos on 23/04/21.
+//  Copyright © 2021 Patricia Carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i-am-rich/README.md
@@ -0,0 +1,10 @@
+# i am rich
+my first app on Xcode
+### by d_llirium
+
+. with comments on how the methods work
+. appIcon 
+. basic StoryBoard
+
+![](https://github.com/d-llirium/I-am-Rich/blob/master/iAMrich.gif?raw=true)
+<img src="https://github.com/d-llirium/I-am-Rich/blob/master/Screen%20Shot%202021-07-07%20at%2020.41.48.png?raw=true" width="200" height="400">
